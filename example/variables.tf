79d188e11e9079fded4920d011cb2e0de436975d
Marco Antonio Blanco <marcoantonio.blanco@nearform.com>
2025-02-12 09:24:04 +0100

    fix: remove useless variables

--- example/variables.tf
+++ example/variables.tf
@@ -1,35 +1,12 @@
 # Commons
-variable "project_name" {
-  type = string
-}
 variable "environment" {
   type    = string
   default = "dev"
 }
+
 variable "aws_region" {
   type    = string
   default = "eu-west-1"
-}
-
-# VPC module
-variable "vpc_id" {
-  type = string
-}
-variable "database_subnets" {
-  type        = list(string)
-  description = "List of IDs of database subnets"
-}
-variable "database_subnet_group_name" {
-  type        = string
-  description = "Name of database subnet group"
-}
-variable "private_subnets" {
-  type        = list(string)
-  description = "List of IDs of private subnets"
-}
-variable "public_subnets" {
-  type        = list(string)
-  description = "List of IDs of public subnets"
 }
 
 # SonarQube module
