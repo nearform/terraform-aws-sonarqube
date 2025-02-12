a5f0dc3bea29f7551f90db28001786b4d8adcc26
Marco Antonio Blanco <marcoantonio.blanco@nearform.com>
2025-02-12 12:26:22 +0100

    refactor: tags

--- example/variables.tf
+++ example/variables.tf
@@ -1,17 +1,4 @@
-# Commons
-variable "environment" {
-  type    = string
-  default = "dev"
-}
-
 variable "aws_region" {
   type    = string
   default = "eu-west-1"
-}
-
-# SonarQube module
-variable "sonar_port" {
-  description = "The port on which SonarQube will run"
-  type        = number
-  default     = 9000
 }
